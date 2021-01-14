2f8de8ef361c0d303ff2772d86c81762314d56da
lalala5772 <65425885+lalala5772@users.noreply.github.com>
2021-01-14 18:19:36 +0900

    Update HNavigarion.js
    <a> tag안에 속성이 없어서 warning 뜨는 거 해결

--- oj_navigation/src/HNavigarion.js
+++ oj_navigation/src/HNavigarion.js
@@ -6,31 +6,31 @@ function HNavigation(){
         <h3>Eucalyptus</h3>
         <nav className="nav">
           <ul className="nav_menu">
-            <li className="nav_menu-item"><a>Home</a></li>
-            <li className="nav_menu-item"><a>Problems</a>
+            <li className="nav_menu-item"><a href={() => false}>Home</a></li>
+            <li className="nav_menu-item"><a href={() => false}>Problems</a>
               <ul className="nav_submenu">
-                <li className="nav_submenu-item"> <a>즐겨찾기 목록</a></li>
-                <li className="nav_submenu-item"> <a>어쩌구</a></li>
-                <li className="nav_submenu-item"> <a>저쩌구</a></li>
-                <li className="nav_submenu-item"> <a>이런저런 문제들</a></li>
+                <li className="nav_submenu-item"> <a href={() => false}>즐겨찾기 목록</a></li>
+                <li className="nav_submenu-item"> <a href={() => false}>어쩌구</a></li>
+                <li className="nav_submenu-item"> <a href={() => false}>저쩌구</a></li>
+                <li className="nav_submenu-item"> <a href={() => false}>이런저런 문제들</a></li>
               </ul>
             </li>
-            <li className="nav_menu-item"><a>Problems</a>
+            <li className="nav_menu-item"><a href={() => false}>Problems</a>
               <ul className="nav_submenu">
-                <li className="nav_submenu-item"> <a>이런문제</a></li>
-                <li className="nav_submenu-item"> <a>저런문제</a></li>
-                <li className="nav_submenu-item"> <a>어쩌구문제</a></li>
-                <li className="nav_submenu-item"> <a>저쩌구문제</a></li>
+                <li className="nav_submenu-item"> <a href={() => false}>이런문제</a></li>
+                <li className="nav_submenu-item"> <a href={() => false}>저런문제</a></li>
+                <li className="nav_submenu-item"> <a href={() => false}>어쩌구문제</a></li>
+                <li className="nav_submenu-item"> <a href={() => false}>저쩌구문제</a></li>
               </ul>
             </li>
-            <li className="nav_menu-item"><a>ITCPC</a>
+            <li className="nav_menu-item"><a href={() => false}>ITCPC</a>
               <ul className="nav_submenu">
-                <li className="nav_submenu-item"> <a>Score</a></li>
-                <li className="nav_submenu-item"> <a>Problem</a></li>
-                <li className="nav_submenu-item"> <a>etc..</a></li>
+                <li className="nav_submenu-item"> <a href={() => false}>Score</a></li>
+                <li className="nav_submenu-item"> <a href={() => false}>Problem</a></li>
+                <li className="nav_submenu-item"> <a href={() => false}>etc..</a></li>
               </ul>
             </li>
-            <li className="nav_menu-item"><a>etc</a></li>
+            <li className="nav_menu-item"><a href={() => false}>etc</a></li>
           </ul>
         </nav>
       </div>
